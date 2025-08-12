98a61aa1c36c022e6fa8550c1d3fb7f407111852
twyutguiwrtguwrytgyrwtgywrtygy <Jacehawkins951@gmail.com>
2025-08-11 22:19:24 -0500

    Create fullscreenjs.js

---
+++ fullscreenjs.js
@@ -0,0 +1,14 @@
+    function openFullscreen() {
+        if (elem.requestFullscreen) {
+            elem.requestFullscreen();
+        } else if (elem.mozRequestFullScreen) { /* Firefox */
+            elem.mozRequestFullScreen();
+        } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari & Opera */
+            elem.webkitRequestFullscreen();
+        } else if (elem.msRequestFullscreen) { /* IE/Edge */
+            elem.msRequestFullscreen();
+        }
+    }
+
+    // Attach this function to a user interaction, e.g., a button click
+    document.getElementById("fullscreenButton").addEventListener("click", openFullscreen);
