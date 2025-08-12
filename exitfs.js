27be10091fab28d415d12adbf03bb357b1731df0
twyutguiwrtguwrytgyrwtgywrtygy <Jacehawkins951@gmail.com>
2025-08-11 22:19:48 -0500

    Create exitfs.js

---
+++ exitfs.js
@@ -0,0 +1,11 @@
+function closeFullscreen() {
+    if (document.exitFullscreen) {
+        document.exitFullscreen();
+    } else if (document.mozCancelFullScreen) { /* Firefox */
+        document.mozCancelFullScreen();
+    } else if (document.webkitExitFullscreen) { /* Chrome, Safari and Opera */
+        document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) { /* IE/Edge */
+        document.msExitFullscreen();
+    }
+}
